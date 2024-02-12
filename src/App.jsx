608b052478e4b681445b771e3d981cf0856a289f
Tuxa4Life <nikoloztuxa@gmail.com>
2024-02-12 11:32:33 +0400

    Generating correct field

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
     const randomizeField = (field) => {
         const width = field.length
         const height = field[0].length
-        let bombCount = 38
+        let bombCount = 28
         
         while (bombCount !== 0) {
             const randomRow = Math.floor(Math.random() * height) // 14
@@ -47,7 +47,18 @@ const App = () => {
             for (let t = 0; t < field[0].length; t++) {
                 if (field[i][t] === -1) continue
                 let value = 0
-                
+
+                if (i < field.length - 1) if (field[i + 1][t] === -1) value ++ // right
+                if (t < field[0].length - 1) if (field[i][t + 1] === -1) value ++ // down
+
+                if (i > 0) if (field[i - 1][t] === -1) value ++ // left
+                if (t > 0) if (field[i][t - 1] === -1) value ++ // top
+
+                if (i < field.length - 1 && t <  field[0].length - 1) if (field[i + 1][t + 1]) value ++ // bottom right
+                if (i > 0 && t >  0) if (field[i - 1][t - 1] === -1) value ++ // top left
+
+                if (i < field.length - 1 && t > 0) if (field[i + 1][t - 1] === -1) value ++ // top right
+                if (i > 0 && t < field[0].length) if (field[i - 1][t + 1] === -1) value ++ // bottom left
 
                 field[i][t] = value
             }
